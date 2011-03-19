7341b90046595a4a3803cd75924c2709e655606e
rajupcomer <rajupcomer@gmail.com>
2011-03-19 14:10:46 +0530

    first commit

---
+++ app/models/state.rb~
@@ -0,0 +1,6 @@
+class State < ActiveRecord::Base
+  belongs_to :country
+  has_many :cities
+  accepts_nested_attributes_for :cities
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redodemo2::Application.config.secret_token = '37217160f5db4b7dee940a27c0276462c2235eba5cfde258616579a16b15a68e9588f437b8a7d1f60fea2c5d10b6bfd0729093dd9d68bea9b0dd56c6efd23f1e'
